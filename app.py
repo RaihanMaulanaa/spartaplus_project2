68f631c8edb5f3f3af4dbf60a3b2ede225dbd86b
RaihanMaulanaa <raihanmaulanasyahputraa@gmail.com>
2023-03-01 12:10:01 +0700

    update

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
-db = client(DB_NAME)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
